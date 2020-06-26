c5ed55badf64f4af4766b3a1d81c35fe98ea6341
azevedorafael <you@example.com>
2020-06-25 23:40:57 -0300

    Add Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Rafael Azevedo</h1>
+    <h2>Programador Front End</h2>
+    <p>Membro da equipe, leitor de stack overflow e solucionador de bugs.</p>
+  </div>
+)
+
+export default Profile
