b6eb0d28dd6327587536c6367f346ee5822dc251
azevedorafael <you@example.com>
2020-07-10 16:10:36 -0300

    Add Algolia plugin settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config()
+
+const queries = require("./src/utils/algolia_queries")
+
 module.exports = {
   siteMetadata: {
     title: `Rafael de Azevedo`,
@@ -53,6 +57,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+        matchFields: ["slug", "modified"],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
