a277411a4e709fa59e8c12c842320d20d42494b7
azevedorafael <you@example.com>
2020-07-13 17:32:54 -0300

    Add function to change the visualization Mode

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Home, Grid } from "styled-icons/boxicons-solid"
+import { ThList as List } from "styled-icons/typicons"
 import {
   SearchAlt2 as Search,
   UpArrowAlt as Arrow,
@@ -10,12 +11,17 @@ import * as S from "./styles"
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === "dark"
+  const isListMode = display === "list"
 
   useEffect(() => {
     setTheme(window.__theme)
+    setDisplay(window.__display)
+
     window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
   return (
@@ -42,8 +48,13 @@ const MenuBar = () => {
         >
           <Light />
         </S.MenuBarItem>
-        <S.MenuBarItem title="Mudar visualização">
-          <Grid />
+        <S.MenuBarItem
+          title="Mudar visualização"
+          onClick={() => {
+            window.__setPreferredDisplay(isListMode ? "grid" : "list")
+          }}
+        >
+          {isListMode ? <Grid /> : <List />}
         </S.MenuBarItem>
         <S.MenuBarItem title="Ir para o Topo">
           <Arrow />
