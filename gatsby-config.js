02e88ed240c102371ae969effefd170c13cb164c
azevedorafael <you@example.com>
2020-07-15 16:45:56 -0300

    Change site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     position: `Developer`,
     description: `A blog about frontend development and other cool stuff.`,
     author: `@myblog`,
-    siteUrl: `https://johndoe.com.br`,
+    siteUrl: `https://rafazevedo.netlify.app`,
   },
   plugins: [
     `gatsby-plugin-transition-link`,
